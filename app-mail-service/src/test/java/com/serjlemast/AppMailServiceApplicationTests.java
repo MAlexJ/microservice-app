2870f6baed2321f1a36b61daaee2a67fb421fb73
serjlemast <maksiomrpp@gmail.com>
2023-10-11 22:48:14 +0300

    Replace groovy to java

---
+++ app-mail-service/src/test/java/com/serjlemast/AppMailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serjlemast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
