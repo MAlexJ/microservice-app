1bbdbe0127ac01091272bbc31b3c1a67ccd360b2
malexj <alexmaximovj@gmail.com>
2023-10-15 21:15:50 +0300

    Add listener to stirage-service for bill status

--- app-storage-service/src/main/java/com/malexj/listener/BillEventListener.java
+++ app-storage-service/src/main/java/com/malexj/listener/BillEventListener.java
@@ -1,6 +1,7 @@
 package com.malexj.listener;
 
 import com.malexj.entity.Bill;
+import com.malexj.entity.BillStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
@@ -15,9 +16,19 @@ public class BillEventListener {
         log.info("Save new bill: {}", bill);
     }
 
+    @HandleBeforeCreate
+    public void handleBeforeCreate(BillStatus status) {
+        log.info("Save new status: {}", status);
+    }
+
     @HandleAfterCreate
     public void handleAfterCreate(Bill bill) {
         log.info("Bill was successfully saved: {}", bill);
+    }
+
+    @HandleAfterCreate
+    public void handleAfterCreate(BillStatus status) {
+        log.info("Status was successfully saved: {}", status);
     }
 
 }
