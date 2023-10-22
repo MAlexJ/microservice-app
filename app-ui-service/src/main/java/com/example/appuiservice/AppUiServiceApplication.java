0531e0e9567de4f420effa7604bd0390e287a090
malexj <alexmaximovj@gmail.com>
2023-10-22 08:20:30 +0300

    Init UI service

---
+++ app-ui-service/src/main/java/com/example/appuiservice/AppUiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.appuiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppUiServiceApplication.class, args);
+    }
+
+}
---
+++ app-ui-service/src/test/java/com/example/appuiservice/AppUiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appuiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppUiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
